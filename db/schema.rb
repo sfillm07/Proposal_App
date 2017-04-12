9754bc9feff2f6ef5cd9070fec519dc76071c437
Sean Fillmore <sfillm07@yahoo.com>
2017-04-12 08:15:19 -0700

    Added name, phone, and email to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412064702) do
+ActiveRecord::Schema.define(version: 20170412150718) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 20170412064702) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "user_cell"
+    t.string   "user_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
